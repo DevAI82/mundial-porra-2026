b4d2827de5db9bff93a6a11e95ff109cf63a7f5c
DevAI82 <jmolmeda@gmail.com>
2026-06-05 23:51:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'porra2026-v2';
+const CACHE = 'porra2026-v3';
 const FILES = ['./', './index.html', './manifest.json', './imagenapp.jpg', './trofeo.png'];
 
 self.addEventListener('install', e => {
