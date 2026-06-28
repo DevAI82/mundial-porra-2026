b5e563290bd39029c5e8ac013fb6d6c3c6451739
Jose <jm_olmeda@hotmail.com>
2026-06-29 01:31:11 +0200

    feat: actualizacion de la app y service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'porra2026-v7';
+const CACHE = 'porra2026-v8';
 const FILES = ['./', './index.html', './manifest.json', './imagenapp.jpg', './trofeo.png'];
 
 self.addEventListener('install', e => {
